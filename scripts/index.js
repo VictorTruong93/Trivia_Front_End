4f6e55421894462541fbff7392cf0e42bac59a6e
Christopher Childers <christopher.childers13@yahoo.com>
2019-03-14 16:27:48 -0400

    fails at getting the trivia data. needs to be worked on

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,45 @@
-const scienceURL = "https://my-little-cors-proxy.herokuapp.com/https://opentdb.com/api.php?amount=20&category=17&difficulty=easy&type=multiple";
+let scienceURL = "https://my-little-cors-proxy.herokuapp.com/https://opentdb.com/api.php?amount=1&category=17&difficulty=easy&type=multiple";
+
+let data = [];
+let triviaGame = [];
+
+//we need a function that will grab the questions for us
+function getMyTriviaData(findMyTrivia){
+    let myQuestion = findMyTrivia.data[0].question;
+    let correctAnswer = findMyTrivia.data[0].correct_answer;
+    let incorrectAnswers = findMyTrivia.data[0].incorrect_answers;
+    console.log(myQuestion)
+
+    triviaGame.append(myQuestion)
+    return myQuestion, correctAnswer, incorrectAnswers;   
+
+}
+
+// //we need a function that will grab the answers for us
+// function getMyAnswer(findCorrectAnswer){
+//     let correctAnswer = findCorrectAnswer.results[0].correct_answer;
+//     return correctAnswer;
+// };
+
+// function getMyFakes(findIncorrectAnswers){
+//     let incorrectAnswers = findIncorrectAnswers.results[0].incorrect_answers;
+//     return incorrectAnswers;
+// };
+// function main(){
+//     getMyQuestion();
+//     getMyAnswer();
+//     getMyFakes();
+// };
 
 function getQs(url) {
+    //fetchs our API data
     fetch(url)
     .then(function(response) {
-        console.log(response.json());
+        //returns the API data as JSON
+        // return response.json();
+        let data = response.json();
+        console.log(data);
+        return data;
     })
-}
+    getMyTriviaData(data);
+};
